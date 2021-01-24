93182ae17449ab745665e14d9ae7c94fe980640b
archibracho <70370377+archibracho@users.noreply.github.com>
2021-01-24 19:22:41 +0300

    Исправлены мелкие ошибки в проекте

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,53 @@
-const headerNavToggle = document.querySelector(".page-header__toogle");
-const headerNav = document.querySelector(".header-nav");
+// Gamburger
 
-headerNavToggle.addEventListener("click", function (evt) {
+const mobileToogle = document.querySelector(".page-header__toogle-nojs");
+const mobileNav = document.querySelector(".header-nav");
+
+mobileToogle.classList.add('page-header__toogle');
+mobileNav.classList.add('header-nav--closed');
+
+mobileToogle.addEventListener("click", function (evt) {
   evt.preventDefault();
-  headerNav.classList.add("header-nav--opened");
+  mobileToogle.classList.toggle("page-header__toogle-nojs--opened");
+  mobileToogle.classList.toggle("page-header__toogle-nojs--closed");
+  mobileNav.classList.toggle("header-nav--closed");
+});
+
+
+// modal
+
+const cartLink = document.querySelector(".product__add-in-basket");
+const modal = document.querySelector(".modal__cart");
+const overlay = document.querySelector(".page__background-modal");
+const buttonCart = document.querySelector(".product-main__button");
+
+cartLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal__cart--opened");
+  overlay.classList.add("page__background-modal--show");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal__cart--opened");
+  overlay.classList.remove("page__background-modal--show");
+});
+
+buttonCart.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal__cart--opened");
+  overlay.classList.add("page__background-modal--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal__cart--opened")) {
+      evt.preventDefault();
+      modal.classList.remove("modal__cart--opened");
+    }
+    if (overlay.classList.contains("page__background-modal--show")) {
+      evt.preventDefault();
+      overlay.classList.remove("page__background-modal--show");
+    }
+  }
 });
