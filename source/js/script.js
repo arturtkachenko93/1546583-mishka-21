4bdbec38732ae58da6b35382b375c2f72754d27f
archibracho <70370377+archibracho@users.noreply.github.com>
2021-01-25 03:19:42 +0300

    Исправлены мелкие ошибки; исправлена разметка модального окна

--- source/js/script.js
+++ source/js/script.js
@@ -3,8 +3,8 @@
 const mobileToogle = document.querySelector(".page-header__toogle-nojs");
 const mobileNav = document.querySelector(".header-nav");
 
-mobileToogle.classList.add('page-header__toogle');
-mobileNav.classList.add('header-nav--closed');
+mobileToogle.classList.add("page-header__toogle");
+mobileNav.classList.add("header-nav--closed");
 
 mobileToogle.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -16,38 +16,65 @@ mobileToogle.addEventListener("click", function (evt) {
 
 // modal
 
-const cartLink = document.querySelector(".product__add-in-basket");
-const modal = document.querySelector(".modal__cart");
+
+const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".page__background-modal");
+const cartLink = document.querySelector(".product__add-in-basket");
 const buttonCart = document.querySelector(".product-main__button");
-
-cartLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.add("modal__cart--opened");
+const openModal = function () {
+  modal.classList.add("modal--opened");
   overlay.classList.add("page__background-modal--show");
-});
+};
 
-overlay.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.remove("modal__cart--opened");
+const closeModal = function () {
+  modal.classList.remove("modal--opened");
   overlay.classList.remove("page__background-modal--show");
-});
+};
 
-buttonCart.addEventListener("click", function (evt) {
+if (cartLink) {
+  cartLink.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal.classList.add("modal__cart--opened");
-  overlay.classList.add("page__background-modal--show");
-});
+  openModal();
+  });
+
+  overlay.addEventListener("click", function (evt) {
+  closeModal();
+  });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (modal.classList.contains("modal__cart--opened")) {
-      evt.preventDefault();
-      modal.classList.remove("modal__cart--opened");
+    if (modal.classList.contains("modal--opened")) {
+    evt.preventDefault();
+    modal.classList.remove("modal--opened");
     }
-    if (overlay.classList.contains("page__background-modal--show")) {
-      evt.preventDefault();
-      overlay.classList.remove("page__background-modal--show");
+
+  if (overlay.classList.contains("page__background-modal--show")) {
+    evt.preventDefault();
+    overlay.classList.remove("page__background-modal--show");}
     }
-  }
+  });
+}
+
+if (buttonCart) {
+  buttonCart.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  openModal();
 });
+
+overlay.addEventListener("click", function (evt) {
+closeModal();
+});
+
+window.addEventListener("keydown", function (evt) {
+
+if (evt.keyCode === 27) {
+if (modal.classList.contains("modal--opened")) {
+  evt.preventDefault();
+  modal.classList.remove("modal--opened");
+  }
+
+if (overlay.classList.contains("page__background-modal--show")) {
+  evt.preventDefault();
+  overlay.classList.remove("page__background-modal--show");}}
+  });
+}
